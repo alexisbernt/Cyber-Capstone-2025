58d2665ff1b2ff4fd8cdac26346276dd4a39c55e
Alexis Bernthal <129408149+alexisbernt@users.noreply.github.com>
2025-02-14 09:13:15 -0600

    connecting front-end to back-end

--- v1/personal-cybersecurity/src/pages/Home.js
+++ v1/personal-cybersecurity/src/pages/Home.js
@@ -3,10 +3,18 @@ import Hero from "../components/layout/home/Hero";
 import Features from "../components/layout/home/Features";
 import ProductShowcase from "../components/layout/home/ProductShowcase";
 import '../App.css';
+import { useState } from "react";
 
 export default function Home() {
+  const [name, setName] = useState('');
+  fetch("http://localhost:3001/").then((results) => {
+    results.json().then((j) => {
+      setName(j.name);
+    })
+  });
   return (
     <div className="bg-black min-h-screen">
+      {name}
       <Header />
       <main>
         <Hero />
