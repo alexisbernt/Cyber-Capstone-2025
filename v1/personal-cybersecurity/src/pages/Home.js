0cf55e5e0292c34832724abb26cebe1f9512c8b8
Alexis Bernthal <129408149+alexisbernt@users.noreply.github.com>
2025-02-18 17:01:31 -0600

    Add files via upload

--- v1/personal-cybersecurity/src/pages/Home.js
+++ v1/personal-cybersecurity/src/pages/Home.js
@@ -1,21 +1,31 @@
-import Header from "../components/Header";
+// import Header from "../components/Header";
 import Hero from "../components/layout/home/Hero";
 import Features from "../components/layout/home/Features";
 import ProductShowcase from "../components/layout/home/ProductShowcase";
 import '../App.css';
 import { useState } from "react";
 
+
 export default function Home() {
   const [name, setName] = useState('');
+
   fetch("http://localhost:3001/").then((results) => {
     results.json().then((j) => {
       setName(j.name);
-    })
+    });
   });
+
   return (
     <div className="bg-black min-h-screen">
       {name}
-      <Header />
+      {/* <Header /> */}
+
+      {/* Banner Image */}
+      <div className="w-full">
+      <img src="/banner_lcc.png" alt="Banner"  
+      style={{ width: "100%", height: "300px", objectFit: "cover" }} />
+      </div>
+
       <main>
         <Hero />
         <Features />
