0b57a1ee76dc04933e2ab7b55d08c1c0d5342d51
Alexis Bernthal <129408149+alexisbernt@users.noreply.github.com>
2025-02-24 11:54:09 -0600

    connecting blog1

--- v1/personal-cybersecurity/src/App.js
+++ v1/personal-cybersecurity/src/App.js
@@ -10,6 +10,7 @@ import Blog from "./pages/Blog";
 // import { QueryClientProvider } from ".tanstack/react-query"; -- 3001 server (which is my back-end) 
 import { Toaster } from "./components/ui/toaster";
 import NotFound from "./pages/NotFound";
+import BlogPage1 from "./pages/parts/BlogPage1";
 
 function App() {
   return (
@@ -32,6 +33,7 @@ function App() {
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/blog" element={<Toaster />} />
+          <Route path="/blog/why-cybersecurity" element={<BlogPage1 />} />
           <Route path="*" element={<NotFound />} /> 
         </Routes>
       </div>
