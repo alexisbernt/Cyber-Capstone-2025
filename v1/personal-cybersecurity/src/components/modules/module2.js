2c5cc45c1b3e9b11030bfd634a5acf48f21779cb
Alexis Bernthal <129408149+alexisbernt@users.noreply.github.com>
2025-04-06 17:09:53 -0500

    Mod 2 side by side

--- v1/personal-cybersecurity/src/components/modules/module2.js
+++ v1/personal-cybersecurity/src/components/modules/module2.js
@@ -26,7 +26,7 @@ export default function Module2() {
     <div className="bg-black min-h-screen">
       <main>
 
-      {/* Flex Container for Left Image & Right Hero Section */}
+      {/* Flex Container for Left Image & Right Content Section */}
       <div className="split-container">
         {/* Left-side Image */}
         <div className="split-left">
@@ -50,6 +50,30 @@ export default function Module2() {
           The more characters in a password, the more possible combinations
           exist, making it harder to crack.
         </p>
+        </div>
+      </div>
+
+      {/* Flex Container for Left Image & Right Content Section */}
+      <div className="split-container">
+        {/* Left-side Content */}
+        <div className="split-left">
+        <p>
+        <h2>Selecting a Good Password.</h2> <br />
+        The more characters in a password, the more possibility of combinations. 
+        You want to create passwords that are 12 to 18 characters in length. This means that for better password protection and management, 
+        it is recommended to use a password manager. 
+        A common password manager is Bitwarden. 
+        Bitwarden is suitable on phones and computers (basically any device that you want to use).
+        </p>
+        </div>
+
+        {/* Right-side Image Section */}
+        <div className="split-right">
+        <img 
+            src="/bitwarden-phone.png"  
+            alt="Left-side Image"  
+            className="split-image"
+          />
         </div>
       </div>
 
