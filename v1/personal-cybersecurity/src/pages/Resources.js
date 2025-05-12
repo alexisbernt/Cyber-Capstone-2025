8e4d4a52d490270a6fdbc4183a93abaf2af62ba7
Alexis Bernthal <129408149+alexisbernt@users.noreply.github.com>
2025-05-12 10:32:31 -0500

    Updated the resources page

--- v1/personal-cybersecurity/src/pages/Resources.js
+++ v1/personal-cybersecurity/src/pages/Resources.js
@@ -1,51 +1,35 @@
-
 import '../css-sections/about.css';
-import deviceImage1 from "../assets/device-2.png"
-import deviceImage2 from "../assets/device-3.png"
-import arrowupright from "../assets/arrow-up-right.svg"
-
+import deviceImage1 from "../assets/device-2.png";
+import deviceImage2 from "../assets/device-3.png";
+import arrowupright from "../assets/arrow-up-right.svg";
 
 const AboutUs = () => {
   return (
-    <section className="about-us" id="about-us">
-      <div className="about-us__image" style={{ position: 'left', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <div className="about-us__image-sushi3">
-          <img src={deviceImage1} alt="computer" data-aos="fade-right" />
+    <div className="login-page">
+      <div className="login-card about-card">
+        <div className="about-images">
+          <img src={deviceImage1} alt="Device 1" className="about-img" />
+          <img src={deviceImage2} alt="Device 2" className="about-img" />
         </div>
 
-        <button className="about-us__button" style={{
-          position: 'absolute',
-          background: 'black',
-          border: '2px solid var(--secondary-color)',
-          borderRadius: '50%',
-          width: '180px',
-          height: '80px',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          cursor: 'pointer'
-        }}>
-          Learn More
-          <img src={arrowupright} alt="learn more" />
-        </button>
-
-        <div className="about-us__image-sushi2">
-          <img src={deviceImage2} alt="iPad" data-aos="fade-right" />
+        <div className="about-content">
+          <p className="login-subtitle">About Cyber Club / 01000011 01111001 01100010 01100101 01110010 01110011 01100101 01100011 01110101 01110010 01101001 01110100 01111001</p>
+          <h1 className="login-title">The Goal of LCC</h1>
+          <p className="sushi__description">
+            The mission of <em>Lexi's Cyber Club</em> is to bring you cybersecurity knowledge that will help you personally and get you to live with ease.
+          </p>
+          <a href="http://localhost:3000/modules" className="login-button about-button">
+            Learn More
+            <img src={arrowupright} alt="Arrow" className="about-arrow" />
+          </a>
         </div>
       </div>
-
-      <div className="about-us__content" data-aos="fade-left">
-        <p className="sushi__subtitle">About Us / 私たちに関しては</p>
-        <h3 className="sushi__title">Our mission is to bring true Japanese flavours to you.</h3>
-        <p className="sushi__description">
-          We will continue to provide the experience of Omotenashi, the Japanese mindset of hospitality, with our shopping and dining for our customers.
-        </p>
-      </div>
-    </section>
+    </div>
   );
 };
 
 export default AboutUs;
+
 
 // const AboutUs = () => {
 //   return (
